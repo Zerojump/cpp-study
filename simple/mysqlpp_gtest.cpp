db29503a481fd9dfc57aef7559545a40762390d1
edgechen <bycmy@qq.com>
2020-06-19 06:49:25 +0800

    mysql

--- simple/mysqlpp_gtest.cpp
+++ simple/mysqlpp_gtest.cpp
@@ -1,23 +1,37 @@
 //
 // Created by edge on 2020/6/5.
+// https://www.jianshu.com/p/efdafe4322b9
+// https://tangentsoft.com/mysqlpp/home
 //
 #include <mysql++/mysql++.h>
+#include <mysql++/ssqls.h>
+#include <gtest/gtest.h>
 #include <iostream>
+#include <vector>
+#include <mysql.h>
 
 using namespace std;
 
-int main(int argc, const char *argv[]) {
+TEST(MySQLTest, SimpleQuery) {
     const char *db = 0, *server = 0, *user = 0, *password = "";
     db = "test";
-    server = "172.17.0.1";
+    server = ""
+             ""
+             ""
+             "";
     user = "root";
     password = "123456";
 
     mysqlpp::Connection conn(false);
     if (conn.connect(db, server, user, password)) {
         cout << "connect db succeed. " << endl;
-        mysqlpp::Query query = conn.query("SELECT * FROM city");
-        if (mysqlpp::StoreQueryResult res = query.store()) {
+        mysqlpp::Query query = conn.query("SELECT * FROM city where id = %1 and cname = %0q");
+        query.parse();
+        mysqlpp::StoreQueryResult res = query.store("sz", "1");
+
+        // mysqlpp::Query query = conn.query("SELECT * FROM city");
+        // mysqlpp::StoreQueryResult res = query.store();
+        if (res) {
             cout.setf(ios::left);
             cout << setw(10) << "id" <<
                  setw(10) << "cname" << endl;
@@ -33,5 +47,40 @@ int main(int argc, const char *argv[]) {
     } else {
         cout << "connect db fail. " << endl;
     }
-    return 0;
+}
+
+typedef struct __city {
+    uint32_t id;
+    string cname;
+} city;
+
+TEST(MySQLTest, STLQuery) {
+    const char *db = 0, *server = 0, *user = 0, *password = "";
+    db = "test";
+    server = "172.17.0.1";
+    user = "root";
+    password = "123456";
+
+    mysqlpp::Connection conn(false);
+    if (conn.connect(db, server, user, password)) {
+        cout << "connect db succeed. " << endl;
+        mysqlpp::Query query = conn.query("SELECT * FROM city");
+        vector<mysqlpp::Row> rows;
+        query.storein(rows);
+        int count(0);
+        for (auto &citie : rows) {
+            int id = (int) citie.at(0);
+            string cname = (string) citie.at(1);
+            printf("%d,%s\n", id, cname.c_str());
+            count += id;
+        }
+        printf("count:%d\n", count);
+    } else {
+        cout << "connect db fail. " << endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);    //  初始化gtest
+    return RUN_ALL_TESTS();                    //  运行所有测试用例
 }
